74225154efef983df39eb61de54169901da81264
Brandon Lamb <blamb888@gmail.com>
2021-03-30 16:36:08 +0900

    add fontawesome react support

--- src/components/marker.jsx
+++ src/components/marker.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
 
-const Marker = () => <div className="marker">&nbsp;</div>;
+const element = <FontAwesomeIcon icon={faMapMarkerAlt} />
+const Marker = () => <div className="marker">{element}</div>;
 
 export default Marker;
