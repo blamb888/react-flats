540e757d0655421224b5cd23cf20b27862108e69
Brandon Lamb <blamb888@gmail.com>
2021-03-30 15:08:44 +0900

    add flat_list.jsx

--- src/components/flat_list.jsx
+++ src/components/flat_list.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import Flat from './flat';
 
 class FlatList extends Component {
+  const renderList = () => {
+    return props.flats.map((flat, index) => {
+      return (
+        <Flat
+          flat={flat}
+          key={flat.lat}
+          selected={flat.name === props.selectedFlat.name}
+          index={index}
+          selectedFlat={props.selectedFlat}
+        />
+      );
+    });
+  };
+
   render() {
     return (
-      <div>
-
+      <div className="flat-list">
+        {renderList()}
       </div>
     );
   }
