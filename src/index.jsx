ce53a0adb41feb074e465fc2be6904a5cebecf60
Brandon Lamb <blamb888@gmail.com>
2021-03-30 14:55:19 +0900

    add app.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,12 @@
+// external modules
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// internal modules
 import '../assets/stylesheets/application.scss';
-
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+import App from './components/app';
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<App />, root);
 }
